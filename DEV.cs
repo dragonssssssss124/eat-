670494b4e593f1e0a79eeb91d3a83539072fc25b
saraansh <gannasaraansh@gmail.com>
2026-06-10 16:46:39 -0700

    idk

--- DEV.cs
+++ DEV.cs
@@ -1,44 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class DEV : MonoBehaviour
+public class Dev : MonoBehaviour
 {
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            SceneManager.LoadScene(1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            SceneManager.LoadScene(2);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            SceneManager.LoadScene(3);
-        }
-        if (Input.GetKey(KeyCode.F))
-        {
-            SceneManager.LoadScene(4);
-        }
-        if (Input.GetKey(KeyCode.G))
-        {
-            SceneManager.LoadScene(5);
-        }
-        if (Input.GetKey(KeyCode.H))
-        {
-            SceneManager.LoadScene(6);
-        }
-        if (Input.GetKey(KeyCode.J))
-        {
-            SceneManager.LoadScene(7);
-        }
-        if (Input.GetKey(KeyCode.K))
-        {
-            SceneManager.LoadScene(8);
-        }
+        
     }
-  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
